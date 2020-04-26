83d056847167c7be1d3b69581977b1a565200cc5
marwa azzabi <marwaazzabi90@gmail.com>
2020-04-26 21:53:50 +0100

    Team component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './App.css';
 import Navbar from './ComponentSimplon/navbar'
 import Caroussel from './ComponentSimplon/caroussel'
+import Team from './Team/team'
+import mouna from './Team/mouna.jpg'
+import  cyrine from './Team/cyrine.jpg'
+import  hamza from './Team/hamza.jpg'
+
+import Footer from './footer/footer'
+
+const team = [
+  {name:"Mouna Ayari",poste:"Gerant" ,img:mouna},
+  {name:"Mohammed Seif Hamza",poste:"Formateur" ,img:hamza},
+  {name:"Cyrine",poste:"chef de fabrique" ,img:cyrine}
+    ]
+
+
 
 function App() {
   return (
     <div className="App">
      <Navbar />
-     <Caroussel />
+  <Team Team={team}/>
+     <Footer />
+    
+  
+    
     </div>
   );
 }
